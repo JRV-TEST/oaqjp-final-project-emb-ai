bc69c10a5f6c5c6673da29566ac5abc7e70590ad
^_^ <138815089+JRV-TEST@users.noreply.github.com>
2026-07-18 22:22:06 +0200

    Add docstring to emotion_detector function
    Added a docstring to the emotion_detector function to explain its purpose.

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,6 +1,11 @@
+" Código de Final_Project. "
 import requests
 import json
+
 def emotion_detector(text_to_analyze):
+    """
+    Analiza los sentimientos de la frase pasada por argumento
+    """
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyze } }
